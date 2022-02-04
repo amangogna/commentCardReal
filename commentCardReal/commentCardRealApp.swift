d84f070fe3963982187e9b44472777740b10da0e
Aman <gogna.a@etoncollege.org.uk>
2022-02-04 12:14:20 +0000

    Initial Commit

---
+++ commentCardReal/commentCardRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  commentCardRealApp.swift
+//  commentCardReal
+//
+//  Created by Gogna, Aman (HJRM) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct commentCardRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
